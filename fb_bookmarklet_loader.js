81ea4430659fb3fb756308086638d48f5472d500
Craiel <loft45@gmail.com>
2014-05-06 03:04:02 -0400

    Fix the bookmarklet loader

--- fb_bookmarklet_loader.js
+++ fb_bookmarklet_loader.js
@@ -4,7 +4,7 @@ javascript:(
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('id', 'frozenBattleScript');
-    js.setAttribute('src', 'https://raw.github.com/Craiel/FrozenBattle/master/frozenBattle.js'); */
+    js.setAttribute('src', 'https://raw.github.com/Craiel/FrozenBattle/master/frozenBattle.js');
     document.head.appendChild(js);
  }()
 );
